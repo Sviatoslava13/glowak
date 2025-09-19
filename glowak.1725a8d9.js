a49b36d1e9fffad3d5257ca2d3d9beb5dfd5a234
Sviatoslava13 <109517186+Sviatoslava13@users.noreply.github.com>
2025-09-19 10:56:42 +0300

    Updates

---
+++ glowak.1725a8d9.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=glowak.1725a8d9.js.map
---
+++ glowak.7a66d295.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll(".faq-question").forEach(e=>{e.addEventListener("click",function(){this.classList.toggle("active");let e=this.nextElementSibling;"block"===e.style.display?e.style.display="none":e.style.display="block"})})});
+//# sourceMappingURL=glowak.7a66d295.js.map
