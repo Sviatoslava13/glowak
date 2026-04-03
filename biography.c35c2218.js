a6b59790450f28a355ee679e6d4fac12436ec391
Sviatoslava13 <109517186+Sviatoslava13@users.noreply.github.com>
2026-04-03 16:08:43 +0300

    Updates

--- biography.c35c2218.js
+++ biography.c35c2218.js
@@ -94,54 +94,7 @@
 
     function localRequire(x) {
       var res = localRequire.resolve(x);
-      if (res === false) {
-        return {};
-      }
-      // Synthesize a module to follow re-exports.
-      if (Array.isArray(res)) {
-        var m = {__esModule: true};
-        res.forEach(function (v) {
-          var key = v[0];
-          var id = v[1];
-          var exp = v[2] || v[0];
-          var x = newRequire(id);
-          if (key === '*') {
-            Object.keys(x).forEach(function (key) {
-              if (
-                key === 'default' ||
-                key === '__esModule' ||
-                Object.prototype.hasOwnProperty.call(m, key)
-              ) {
-                return;
-              }
-
-              Object.defineProperty(m, key, {
-                enumerable: true,
-                get: function () {
-                  return x[key];
-                },
-              });
-            });
-          } else if (exp === '*') {
-            Object.defineProperty(m, key, {
-              enumerable: true,
-              value: x,
-            });
-          } else {
-            Object.defineProperty(m, key, {
-              enumerable: true,
-              get: function () {
-                if (exp === 'default') {
-                  return x.__esModule ? x.default : x;
-                }
-                return x[exp];
-              },
-            });
-          }
-        });
-        return m;
-      }
-      return newRequire(res);
+      return res === false ? {} : newRequire(res);
     }
 
     function resolve(x) {
